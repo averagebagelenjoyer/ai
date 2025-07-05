5a72653fe6ea62ff2f612dfb411c254a9f7a3634
averagebagelenjoyer <cloakoverseerb@gmail.com>
2025-07-05 14:44:33 -0700

    Update xorg.sh

--- xorg.sh
+++ xorg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-sudo pacman -S --noconfirm base-devel xorg
+sudo pacman -S --noconfirm base-devel xorg xfce4 xfce4-goodies
 echo "exec startxfce4" > ~/.xinitrc
 sudo reboot
