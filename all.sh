86f560a7c97878bc5e4480993a69764a4df715d7
averagebagelenjoyer <cloakoverseerb@gmail.com>
2025-07-05 14:55:31 -0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-sudo pacman -Syu
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/yay.sh | bash -
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/xorg.sh | bash -
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm curl
+curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/yay.sh | bash -; rm yay.sh
+curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/xorg.sh | bash -; rm xorg.sh
