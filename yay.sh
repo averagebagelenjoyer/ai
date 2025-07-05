26a4f1726b12aba7f68bfcbfeebc960b215f0981
averagebagelenjoyer <cloakoverseerb@gmail.com>
2025-07-05 14:57:02 -0700

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 (cd yay && makepkg -si --noconfirm)
