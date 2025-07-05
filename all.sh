1e67a41402b65c02b6c5792a384087733a16c67e
averagebagelenjoyer <cloakoverseerb@gmail.com>
2025-07-05 14:38:33 -0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -euo pipefail
 sudo pacman -Syu
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/install.sh | bash -
+curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/yay.sh | bash -
+curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/xorg.sh | bash -
