7b39664cbf4ef33d143d5ec3246a2dcd9e09af69
averagebagelenjoyer <cloakoverseerb@gmail.com>
2025-07-06 15:29:52 -0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm curl
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/yay.sh | bash -
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/xorg.sh | bash -
-curl https://raw.github.com/averagebagelenjoyer/archdownloads/main/cyber.sh | bash -
+curl https://raw.githubusercontent.com/averagebagelenjoyer/archdownloads/main/yay.sh | bash -
+curl https://raw.githubusercontent.com/averagebagelenjoyer/archdownloads/main/xorg.sh | bash -
+curl https://raw.githubusercontent.com/averagebagelenjoyer/archdownloads/main/cyber.sh | bash -
